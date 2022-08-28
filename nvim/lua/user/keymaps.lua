36ff1deaf867223cdc1ddf6f00c36d9f999bdd8e
Payam Yavari <payam@yavtech.se>
2022-08-28 16:17:21 +0200

    test

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,19 +1,3 @@
----- Inspiration https://github.com/jonhoo/configs/blob/master/editor/.config/nvim/init.vim
---set encoding=utf-7
---
---call plug#begin()
---Plug 'lunarvim/darkplus.nvim' " Color scheme
---Plug 'preservim/nerdtree'
---call plug#end()
---
---colorscheme darkplus
---set termguicolors
---set mouse=a
---set number
---set tabstop=5 softtabstop=0 expandtab shiftwidth=4 smarttab
---set cursorline
---
-
 local opts = { noremap = true, silent = true }
 
 local term_opts = { silent = true }
@@ -41,8 +25,6 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
-keymap("n", "<leader>e", ":Lex 30<cr>", opts)
-
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
@@ -50,7 +32,7 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-ymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Move text up and down
@@ -58,7 +40,7 @@ keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
 -- Insert --
--- Press jk fast to enter
+-- Press jk fast to exit insert mode 
 keymap("i", "jk", "<ESC>", opts)
 
 -- Visual --
@@ -78,9 +60,4 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
--- Terminal --
--- Better terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-
+keymap("n", "<leader>e", ":Vexplore<CR>", opts)
